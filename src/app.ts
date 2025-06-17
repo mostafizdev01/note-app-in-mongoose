a3689eaff01c2f14b420840b66990566df0fd261
Hera <mostafizhera@gmail.com>
2025-06-17 15:35:45 +0600

    create successfull note app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,29 @@
 import express, {Application, Request, Response} from 'express'
+import { model, Schema } from 'mongoose';
 const app: Application = express();
+
+
+const noteSchema = new Schema({
+    title: String,
+    content: String
+})
+
+const Note = model("Note", noteSchema)
+
+
+app.post('/create-note', async (req: Request, res: Response)=>{
+    const myNote = new Note({
+        title: "Learning Mongoose",
+        content: "I'm Learning Mongodb and Mongoose"
+    })
+    res.status(201).json({
+        success: true,
+        message: "Note created successfully ✅",
+        note: myNote
+    })
+    await myNote.save()
+})
+
 
 app.get('/', (req: Request, res: Response)=>{
     res.send("Wellcome to my webserver..")
